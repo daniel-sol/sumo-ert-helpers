ae0921b0071fb50456f1b47f69b13af955635f1d
Daniel Berge Sollien <daniel.sollien@gmail.com>
2023-05-24 15:56:48 +0200

    Add forward model for bulk upload of summary data

---
+++ scripts/fm_ecl_bulk_export.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import logging
+import argparse
+from ecl2df.bulk import bulk_export_with_configfile
+
+logging.basicConfig(level="DEBUG")
+
+
+def parse_args():
+    """Parse arguments for script
+
+    Returns:
+        argparse.NameSpace: The arguments parsed
+    """
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description=("Export all available datatypes from eclipse run"),
+    )
+    parser.add_argument("eclpath", help="Path to eclipse datafile", type=str)
+    parser.add_argument("fmu_config_path", help="Path to fmu config path", type=str)
+    args = parser.parse_args()
+    return args
+
+
+def export(args):
+    """Export all datatypes from eclipse present
+
+    Args:
+        args (argparse.NameSpace): the arguments parsed by script
+    """
+    bulk_export_with_configfile(args.fmu_config_path, args.eclpath)
+
+
+def main():
+    """Run script"""
+    args = parse_args()
+    export(args)
+
+
+if __name__ == "__main__":
+    main()
